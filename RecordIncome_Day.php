77ea904cea3f17f380d30b73baf464529457a4dc
Chutinan <60294922+Chutinan01426@users.noreply.github.com>
2020-12-13 23:26:38 +0700

    Add files via upload

--- RecordIncome_Day.php
+++ RecordIncome_Day.php
@@ -8,20 +8,8 @@
 </head>
 <body>
    <h1><b>Record income (Day) </b></h1>
-   
-   <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password);
-
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    echo "Connected successfully";
+   <?php 
+    echo"Hello world";
   ?>
 
 </body>
